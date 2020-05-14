833fab56b09db38dfa3b2ff85051347b7d2dbd63
Sam Mo <361754100@qq.com>
2020-05-14 23:27:37 +0800

    code commit

---
+++ src/main/java/com/test/answer/response/CommonWrapper.java
@@ -0,0 +1,36 @@
+package com.test.answer.response;
+
+import com.smart.sperms.enums.ResultCodeEnum;
+
+import java.io.Serializable;
+
+/**
+ * Http 响应消息体父类
+ */
+public class CommonWrapper implements Serializable{
+    /**
+     * 状态码
+     */
+    public int resultCode = ResultCodeEnum.SUCCESS.getCode();
+
+    /**
+     * 处理结果
+     */
+    public String resultMsg = "";
+
+    public int getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(int resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public String getResultMsg() {
+        return resultMsg;
+    }
+
+    public void setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+    }
+}
---
+++ src/main/java/com/test/answer/service/PointsInfoService.java
@@ -0,0 +1,7 @@
+package com.test.answer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PointsInfoService {
+}
